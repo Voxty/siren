d2b2f62bd38a65e48772f04b20668be2bb526275
PikachuGamingOfficial <liambowen72@gmail.com>
2022-11-12 11:13:53 -0500

    Release

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/**/*.ogg'
+}
+
+client_scripts {
+	'Common.Client.net.dll',
+	'Siren.Client.net.dll'
+}
+
+server_scripts {
+	'Common.Server.net.dll',
+	'Siren.Server.net.dll'
+}
